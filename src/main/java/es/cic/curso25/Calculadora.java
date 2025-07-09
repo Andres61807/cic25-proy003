ea8da6768d38dd96de424e07c08651367f376801
noname <mail@example.mail>
2025-07-09 13:41:53 +0200

    test pasados con exito

--- src/main/java/es/cic/curso25/Calculadora.java
+++ src/main/java/es/cic/curso25/Calculadora.java
@@ -10,4 +10,20 @@ public class Calculadora extends Object {
     public void sumar(double valor) {
         total = total + valor;
     }
+
+    public void restar(double valor){
+        total-=valor;
+    }
+
+    public void multiplicar(double valor){
+        total*=valor;
+    }
+
+    public void dividir(double divisor){
+        if(divisor!=0){
+            total/=divisor;
+        } else{
+            total=0;
+        }
+    }
 }
--- src/test/java/es/cic/curso25/CalculadoraTest.java
+++ src/test/java/es/cic/curso25/CalculadoraTest.java
@@ -10,18 +10,45 @@ public class CalculadoraTest {
         // Preparo
         Calculadora cut = new Calculadora();
 
-
         // Ejecuto
         cut.sumar(5.6);
+        cut.sumar(3.4);
 
         // Verifico
         double valorActual = cut.getTotal();
-        assertEquals(5.6, valorActual, 0.000001);
-
-
-        cut.sumar(3.4);
-        valorActual = cut.getTotal();
-
         assertEquals(9, valorActual, 0.000001);
+    }
+
+    @Test
+    public void testRestar(){
+        Calculadora calc=new Calculadora();
+
+        calc.restar(10);
+
+        double resultado=calc.getTotal();
+
+        assertEquals(-10,resultado,0.00001);
+    }
+
+    @Test
+    public void testMultiplicar(){
+        Calculadora calc=new Calculadora();
+
+        calc.multiplicar(10);
+
+        double resultado=calc.getTotal();
+
+        assertEquals(0,resultado,0.00001);
+    }
+
+    @Test
+    public void testDividir(){
+        Calculadora calc=new Calculadora();
+
+        calc.multiplicar(0);
+
+        double resultado=calc.getTotal();
+
+        assertEquals(0,resultado,0.00001);
     }
 }
